9abf7cfa1a3bc65a0bf650ee1dcde1e72a0db916
Dovy <dovy91@gmail.com>
2024-12-05 02:07:00 -0500

    env fix 2

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(({ mode }) => ({
 		outDir: '../dist',
 	},
 	define: {
-		'process.env.API_URL': JSON.stringify(
+		'process.env.API_URL': process.env.API_URL || JSON.stringify(
 			mode === 'development' && useRemoteDebugging ? `http://${localIp}:8000` : `http://localhost:8000`
 		),
 		'process.env.IS_DEV': mode === 'development',
